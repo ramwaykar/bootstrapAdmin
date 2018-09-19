9bd383cff9340ada1f2415f2c11ee0dace912ebf
Rambhau Waykar <ramwaykar@gmail.com>
2018-09-19 19:43:53 +0530

    prevented enter key event for forms

--- static/js/common.js
+++ static/js/common.js
@@ -256,4 +256,9 @@ $(document).ready(function () {
     $(".clear-search-form").click(function () {
         common.clearSearch();
     });
+    $("form").bind("keypress", function (e) {
+        if (e.keyCode == 13) {
+            return false;
+        }
+    });
 });
